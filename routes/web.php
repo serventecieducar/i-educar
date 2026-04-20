6ae2311d6ab173b1a6235bebb0c2aef696c9ab09
jadergabriel <programacaoeti@outlook.com>
2026-04-20 17:20:33 -0300

    refactor: remove buriti/i-educar-catraca-frequencia-package and add integrated attendance report route
    - Removed the buriti/i-educar-catraca-frequencia-package from composer.json.
    - Added a new route for the integrated attendance report in web.php, allowing access to the report with appropriate middleware.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EnrollmentInepController;
 use App\Http\Controllers\EnrollmentsPromotionController;
 use App\Http\Controllers\ExportController;
+use App\Http\Controllers\IntegratedAttendanceReportController;
 use App\Http\Controllers\CleanupEmptyActiveSchoolClassesController;
 use App\Http\Controllers\SchoolClassController;
 use App\Http\Controllers\SocialiteCallbackController;
@@ -199,6 +200,10 @@ Route::group(['middleware' => ['ieducar.navigation', 'ieducar.footer', 'ieducar.
 
     Route::post('/enrollments-promotion', [EnrollmentsPromotionController::class, 'processEnrollmentsPromotionJobs'])
         ->name('enrollments.promotion');
+
+    Route::get('/escola/relatorios-integrados/frequencia-alunos', [IntegratedAttendanceReportController::class, 'index'])
+        ->middleware('can:view:' . Process::INTEGRATED_ATTENDANCE_REPORT)
+        ->name('integrated-attendance-report.index');
 
     Route::fallback([WebController::class, 'fallback']);
 });
